80b473026eb12852ba34391679668d2bba0720f7
Yannik Potdevin <ypo@informatik.uni-kiel.de>
2021-11-26 16:51:01 +0100

    Added dp rMSE and dp quasi-rMSE losses.

--- losses.py
+++ losses.py
@@ -1,21 +1,32 @@
 # ypo@informatik.uni-kiel.de
 
-from typing import Optional
+import logging
+import math
+from typing import Any, Optional, cast
+
 import numpy as np
-from numpy.core.fromnumeric import clip
 from sklearn.ensemble._gb_losses import (BinomialDeviance,
                                          ClassificationLossFunction,
                                          LeastSquaresError, LossFunction,
                                          MultinomialDeviance)
 
-import logging
+from dp_variance import smooth_sensitivity
+from dp_rmse import dp_rMS_cauchy
 
+__all__ = [
+    BinomialDeviance, ClassificationLossFunction, LeastSquaresError,
+    LossFunction, MultinomialDeviance, 'keep_each_tree_predicate',
+    'useful_tree_predicate', 'ClippedLeastSquaresError',
+    'ClippedLSEPredicate', 'ClippedBinomialDeviance',
+    'ClippedMultinomialDeviance', 'RootExpQLeastSquaresError',
+    'RootMedianLeastSquaresError', 'DP_quasi_rMSE',
+]
 logger = logging.getLogger(__name__)
 
 
 def keep_each_tree_predicate(
-        y,
-        raw_predictions,
+        y: Any,
+        raw_predictions: Any,
         previous_loss: float,
         current_loss: float) -> bool:
     """This predicate will always return True.
@@ -24,8 +35,8 @@ def keep_each_tree_predicate(
     return True
 
 def useful_tree_predicate(
-        y,
-        raw_predictions,
+        y: Any,
+        raw_predictions: Any,
         previous_loss: float,
         current_loss: float) -> bool:
     """This predicated tells whether `current_loss < previous_loss`.
@@ -36,7 +47,7 @@ def useful_tree_predicate(
     """
     return current_loss < previous_loss
 
-class ClippedLeastSquaresError(LeastSquaresError):
+class ClippedLeastSquaresError(LeastSquaresError):# type: ignore
     """Loss function for clipped least squares (LS) estimation.
 
     This extension overrides the `LeastSquaresError` method __call__()
@@ -51,11 +62,16 @@ class ClippedLeastSquaresError(LeastSquaresError):
         below.
     """
 
-    def __init__(self, clipping_bound):
+    def __init__(self, clipping_bound: float) -> None:
         super().__init__()
         self.clipping_bound = clipping_bound
 
-    def __call__(self, y, raw_predictions, sample_weight = None):
+    def __call__(
+            self,
+            y: np.ndarray,
+            raw_predictions: np.ndarray,
+            sample_weight: Optional[np.ndarray] = None
+        ) -> float:
         """Compute the clipped least squares loss.
 
         Parameters
@@ -66,16 +82,21 @@ class ClippedLeastSquaresError(LeastSquaresError):
         raw_predictions : ndarray of shape (n_samples, K)
             The raw predictions (i.e. values from the tree leaves).
 
-        sample_weight : ndarray of shape (n_samples,), default=None
+        sample_weight : ndarray of shape (n_samples,), optional
             Sample weights.
         """
         c = self.clipping_bound
         if sample_weight is None:
-            return np.mean(np.clip((y - raw_predictions.ravel()) ** 2, -c, c))
+            return cast(
+                float,
+                np.mean(
+                    np.clip((y - raw_predictions.ravel()) ** 2, -c, c)
+                )
+            )
         else:
             raise NotImplementedError(
-                "Clipping is not implemented if argument `sample_weight` is "
-                "not None."
+                "Clipping is not implemented if argument `sample_weight`"
+                "is not None."
             )
 
     def __repr__(self) -> str:
@@ -104,12 +125,15 @@ class ClippedLSEPredicate():
 
     def __call__(
             self,
-            y,
-            raw_predictions,
+            y: np.ndarray,
+            raw_predictions: Any,
             previous_loss: float,
             current_loss: float) -> bool:
-        lap_noise = self.rng.laplace(
-          scale = self.clipping_bound / (len(y) * self.privacy_budget)
+        lap_noise = cast(
+            float,
+            self.rng.laplace(
+            scale = self.clipping_bound / (len(y) * self.privacy_budget)
+            )
         )
         logger.debug(
             "Performing AboveThreshold mechanism using c = %f, len(y) = %d, "
@@ -126,30 +150,30 @@ class ClippedLSEPredicate():
         return _repr
 
 
-class ClippedBinomialDeviance(BinomialDeviance):
-    def __init__(self, n_classes, clipping_bound):
+class ClippedBinomialDeviance(BinomialDeviance): # type: ignore
+    def __init__(self, n_classes: int, clipping_bound: float) -> None:
         super().__init__(n_classes = n_classes)
         self.clipping_bound = clipping_bound
 
-    def __call__(self, y, raw_predictions, sample_weight = None):
+    def __call__(self, y, raw_predictions, sample_weight = None): # type: ignore
         raise NotImplementedError()
 
-class ClippedMultinomialDeviance(MultinomialDeviance):
-    def __init__(self, n_classes, clipping_bound):
+class ClippedMultinomialDeviance(MultinomialDeviance): # type: ignore
+    def __init__(self, n_classes: int, clipping_bound: float) -> None:
         super().__init__(n_classes = n_classes)
         self.clipping_bound = clipping_bound
 
-    def __call__(self, y, raw_predictions, sample_weight = None):
+    def __call__(self, y, raw_predictions, sample_weight = None): # type: ignore
         raise NotImplementedError()
 
-class RootExpQLeastSquaresError(LeastSquaresError):
+class RootExpQLeastSquaresError(LeastSquaresError): # type: ignore
     def __init__(
             self,
-            lower_bound,
-            upper_bound,
-            privacy_budget,
-            q = 0.5,
-            random_seed = None):
+            lower_bound: float,
+            upper_bound: float,
+            privacy_budget: float,
+            q: float = 0.5,
+            random_seed: Optional[int] = None) -> None:
         super().__init__()
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
@@ -157,10 +181,14 @@ class RootExpQLeastSquaresError(LeastSquaresError):
         self.q = q
         self.random_seed = random_seed
 
-    def __call__(self, y, raw_predictions, sample_weight = None):
+    def __call__(
+            self,
+            y: np.ndarray,
+            raw_predictions: np.ndarray,
+            sample_weight: Optional[np.ndarray] = None) -> float:
         if sample_weight is None:
             # Note that sqrt(median(x_1 ** 2, x_2 ** 2, ..., x_n ** 2))
-            #           = median(|x_1|, |x_2|, ...,  |x_n|)
+            #           = median(|x_1|, |x_2|, ..., |x_n|)
             # (the same holds for quantiles other than q = 0.5).
             abs_devs = np.abs(y - raw_predictions.ravel())
             abs_devs = np.sort(abs_devs)
@@ -188,7 +216,7 @@ class RootExpQLeastSquaresError(LeastSquaresError):
                 "not None."
             )
 
-    def _exp_q(self, x, q = 0.5):
+    def _exp_q(self, x: np.ndarray, q: float = 0.5) -> float:
         """Assume that `x` = [x_min, x_1, x_2, ..., x_n, x_max] and that
         `x` is sorted. Return the median of `x` in a differentially
         private manner."""
@@ -199,34 +227,41 @@ class RootExpQLeastSquaresError(LeastSquaresError):
             privacy_budget = self.privacy_budget
         )
         i = rng.choice(np.arange(len(x) - 1), p = probabilities)
-        return rng.uniform(low = x[i], high = x[i + 1])
+        return cast(float, rng.uniform(low = x[i], high = x[i + 1]))
 
-    def _utilities(self, x, q):
+    def _utilities(self, x: np.ndarray, q: float) -> np.ndarray:
         """Assume that x has length n + 2 (i.e. it contains n elements
         of interest and the lower and upper bound. Return n + 1
         utilities."""
         _x = np.arange(len(x) - 1)
         median_position = np.floor(len(x) * q)
-        utilities = np.where(
-            _x < median_position,
-            _x + 1 - median_position,
-            median_position - _x
+        utilities = cast(
+            np.ndarray,
+            np.where(
+                _x < median_position,
+                _x + 1 - median_position,
+                median_position - _x
+            )
         )
         return utilities
 
-    def _bin_sizes(self, x):
+    def _bin_sizes(self, x: np.ndarray) -> np.ndarray:
         """Assume that `x` = [x_min, x_1, x_2, ..., x_n, x_max] and that
         `x` is sorted. Return n + 1 bin sizes"""
         bin_sizes = x - np.roll(x, 1)
-        return bin_sizes[1:]
+        return  cast(np.ndarray, bin_sizes[1:])
 
-    def _probabilities(self, utilities, bin_sizes, privacy_budget):
+    def _probabilities(self,
+            utilities: np.ndarray,
+            bin_sizes: np.ndarray,
+            privacy_budget: float
+        ) -> np.ndarray:
         utility_ = privacy_budget / 2 * utilities
         # This is for numerical stability:
         max_utility = utility_.max()
         ps = bin_sizes * np.exp(utility_ - max_utility)
         ps /= ps.sum()
-        return ps
+        return cast(np.ndarray, ps)
 
     def __repr__(self) -> str:
         _repr = (
@@ -241,12 +276,20 @@ class RootExpQLeastSquaresError(LeastSquaresError):
         )
         return _repr
 
-class RootMedianLeastSquaresError(LeastSquaresError):
-    def __call__(self, y, raw_predictions, sample_weight = None):
+class RootMedianLeastSquaresError(LeastSquaresError): # type: ignore
+    def __call__(
+            self,
+            y: np.ndarray,
+            raw_predictions: np.ndarray,
+            sample_weight: Optional[np.ndarray] = None
+        ) -> float:
         if sample_weight is None:
             # Note that sqrt(median(x_1 ** 2, x_2 ** 2, ..., x_n ** 2))
             #           = median(|x_1|, |x_2|, ..., |x_n|)
-            return np.median(np.abs(y - raw_predictions.ravel()))
+            return cast(
+                float,
+                np.median(np.abs(y - raw_predictions.ravel()))
+            )
         else:
             raise NotImplementedError(
                 "RMedLSE is not implemented if argument `sample_weight` is "
@@ -256,17 +299,24 @@ class RootMedianLeastSquaresError(LeastSquaresError):
     def __repr__(self) -> str:
         return "RootMedianLeastSquaresError"
 
-class RootQuantileLeastSquaresError(LeastSquaresError):
-    def __init__(self, q):
+class RootQuantileLeastSquaresError(LeastSquaresError): # type: ignore
+    def __init__(self, q: float) -> None:
         super().__init__()
         self.q = q
 
-    def __call__(self, y, raw_predictions, sample_weight = None):
+    def __call__(self,
+            y: np.ndarray,
+            raw_predictions: np.ndarray,
+            sample_weight: Optional[np.ndarray] = None
+        ) -> float:
         if sample_weight is None:
             # Note that sqrt(median(x_1 ** 2, x_2 ** 2, ..., x_n ** 2))
             #           = median(|x_1|, |x_2|, ..., |x_n|),
             # which holds analogously for other quantiles.
-            return np.quantile(np.abs(y - raw_predictions.ravel()), self.q)
+            return cast(
+                float,
+                np.quantile(np.abs(y - raw_predictions.ravel()), self.q)
+            )
         else:
             raise NotImplementedError(
                 "RQuantileLSE is not implemented if argument `sample_weight`"
@@ -276,6 +326,95 @@ class RootQuantileLeastSquaresError(LeastSquaresError):
     def __repr__(self) -> str:
         return "RootQuantileLeastSquaresError(q={})".format(self.q)
 
+class DP_quasi_rMSE(LeastSquaresError): # type: ignore
+    def __init__(self,
+            privacy_budget: float,
+            beta: float,
+            L: float,
+            U: float,
+            m: int,
+            seed: int
+        ) -> None:
+        super().__init__()
+        self.privacy_budget = privacy_budget
+        self.beta = beta
+        self.L = L
+        self.U = U
+        self.m = m
+        self.seed = seed
+
+    def __call__(self,
+            y: np.ndarray,
+            raw_predictions: np.ndarray,
+            sample_weight: Optional[np.ndarray] = None
+        ) -> float:
+        if sample_weight is None:
+            assert (
+                self.privacy_budget > 0 and self.beta > 0
+                and self.m >= 0 and self.L < self.U
+            )
+            sample = y - raw_predictions.ravel()
+            sample.sort()
+            trimmed_sample = sample[self.m : sample.size - self.m]
+            s = smooth_sensitivity(
+                trimmed_sample,
+                0.0,
+                self.beta,
+                self.L,
+                self.U,
+                'std'
+            )
+            scale = math.sqrt(2) * s / self.privacy_budget
+            rng = np.random.default_rng(seed = self.seed)
+            noise = rng.standard_cauchy(size = 1)[0] * scale
+            return cast(float, np.sqrt(np.mean(trimmed_sample ** 2)) + noise)
+        else:
+            raise NotImplementedError(
+                "DP_quasi-rMSE is not implemented if argument `sample_weight`"
+                "is not None."
+            )
+
+    def __repr__(self) -> str:
+        template = "DP_quasi-rMSE(privacy_budget={},beta={},L={},U={},m={})"
+        return template.format(
+            self.privacy_budget,
+            self.beta,
+            self.L,
+            self.U,
+            self.m
+        )
+
+class DP_rMSE(LeastSquaresError):# type: ignore
+    def __init__(self,
+            privacy_budget: float,
+            U: float,
+            seed: Optional[int] = None,
+        ) -> None:
+        super().__init__()
+        self.privacy_budget = privacy_budget
+        self.U = U
+        self.seed = seed
+
+    def __call__(self,
+            y: np.ndarray,
+            raw_predictions: np.ndarray,
+            sample_weight: Optional[np.ndarray] = None
+        ) -> float:
+        if sample_weight is None:
+            return dp_rMS_cauchy(
+                y - raw_predictions.ravel(), self.privacy_budget,
+                self.U, self.seed
+            )
+        else:
+            raise NotImplementedError(
+                "DP_rMSE is not implemented if argument `sample_weight`"
+                "is not None."
+            )
+
+    def __repr__(self) -> str:
+        return "DP-rMSE(privacy_budget={},U={})".format(
+            self.privacy_budget, self.U
+        )
 
 LOSS_FUNCTIONS = {
     'ls'        : LeastSquaresError,
